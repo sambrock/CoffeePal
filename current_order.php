b378e1816d9a3911ca15da0b7a7ab34741ad4ded
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-01-23 00:32:59 +0000

    Added current order.

---
+++ current_order.php
@@ -0,0 +1,16 @@
+<?php
+include("db_conn.php");
+
+$q_order_id="SELECT id FROM orders ORDER BY id DESC LIMIT 1";
+$prep_stmt_order_id=$conn->prepare($q_order_id);
+$prep_stmt_order_id->execute();
+$order_id_arr=$prep_stmt_order_id->fetch();
+
+$order_id=$order_id_arr[0];
+
+$q_current_order="SELECT orders.id as order_id, products.name as item_name, products.price as item_price, orders.date_time FROM orders INNER JOIN order_items ON order_items.order_id=orders.id INNER JOIN products ON order_items.product_id=products.id WHERE orders.id=:order_id";
+$prep_stmt_current_order=$conn->prepare($q_current_order);
+$prep_stmt_current_order->bindValue(":order_id", $order_id);
+$prep_stmt_current_order->execute();
+$current_order=$prep_stmt_current_order->fetchAll();
+?>
