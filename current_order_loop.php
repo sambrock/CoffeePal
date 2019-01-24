82556abab7a6fdcac76daf3081068cb44f517252
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-01-24 22:17:58 +0000

    Added update order items.

---
+++ current_order_loop.php
@@ -0,0 +1,8 @@
+<?php
+include("current_order.php");
+foreach($current_order as $item){
+    echo "<div class='order-item'>";
+    echo "<span class='order-item-name'>{$item["item_name"]}</span><span class='order-item-price'>£{$item["item_price"]}</span><span class='delete' data-order-item-id='{$item["order_item_id"]}'></span>";
+    echo "</div>";
+}
+?>
