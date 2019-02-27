f631cef198225d6da8125aa1714e1fe6b17f7e43
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-02-27 14:56:11 +0000

    Redirect to order.php if logged in.

--- index.php
+++ index.php
@@ -1,23 +1,30 @@
 <?php
-require_once("models/model.php");
+session_start();
 
-$title = "Login";
-include("views/header-view.php");
+if(isset($_SESSION["name"])){
+    header("Location: order.php");
 
-if(isset($_POST['login'])){
-    $id = $_POST['id'];
-    $name = getLogin($id)['first_name']." ".getLogin($id)['last_name'];
-    $password = trim($_POST['password']);
-    $hashed_password = getLogin($id);
+}else{
+    require_once("models/model.php");
+    $title = "Login";
+    include("views/header-view.php");
 
-    if(password_verify($password, $hashed_password['password'])){
-        $_SESSION["id"] = $id;
-        $_SESSION["name"] = $name;
-        header("Location: order.php");
-    } else {
-        $error="Password incorrect";
+    if(isset($_POST['login'])){
+        $id = $_POST['id'];
+        $name = getLogin($id)['first_name']." ".getLogin($id)['last_name'];
+        $password = trim($_POST['password']);
+        $hashed_password = getLogin($id);
+
+        if(password_verify($password, $hashed_password['password'])){
+            $_SESSION["id"] = $id;
+            $_SESSION["name"] = $name;
+            header("Location: order.php");
+        } else {
+            $error="Password incorrect";
+        }
     }
+
+    include("views/login-view.php");
 }
 
-include("views/login-view.php");
 ?>
